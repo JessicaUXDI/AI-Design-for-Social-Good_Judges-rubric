e2fe6ccdbf88b6255ab9b98675733c9667797d24
Itsmejessica <65266218+JessicaUXDI@users.noreply.github.com>
2025-12-01 22:42:40 -0800

    Refactor OPTIONS response handling in get-rankings.js

--- api/get-rankings.js
+++ api/get-rankings.js
@@ -7,8 +7,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
+    return res.status(200).end();
   }
 
   if (req.method !== 'GET') {
